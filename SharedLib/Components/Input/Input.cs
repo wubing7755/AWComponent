cc14d6f8434e10846d2bc99dcbbcea66874db28c
吴冰 <wubing@xuanit.com>
2025-04-09 18:07:18 +0800

    update: input component

--- SharedLib/Components/Input/Input.cs
+++ SharedLib/Components/Input/Input.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Components.Rendering;
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
 
 namespace SharedLibrary.Components;
 
 public class Input : AWComponentBase
 {
-
+    [Parameter]
+    public InputType Type { get; set; }
 
     protected override void BuildComponent(RenderTreeBuilder builder)
     {
@@ -13,9 +15,35 @@ public class Input : AWComponentBase
         builder.OpenElement(seq++, "div");
 
         builder.OpenElement(seq++, "input");
-
+        builder.AddAttribute(seq++, "type", Type);
         builder.CloseElement();
 
         builder.CloseElement();
     }
+}
+
+public enum InputType
+{
+    Button,
+    Checkbox,
+    Color,
+    Date,
+    DatetimeLocal,
+    Email,
+    File,
+    Hidden,
+    Image,
+    Month,
+    Number,
+    Password,
+    Radio,
+    Range,
+    Reset,
+    Search,
+    Submit,
+    Tel,
+    Text,
+    Time,
+    Url,
+    Week
 }
